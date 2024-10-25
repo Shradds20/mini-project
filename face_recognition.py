57c7b3d7d12dfc51b0bb966791556684ce5bff71
Shradds20 <shradds2005gaikwad@gmail.com>
2024-10-25 09:50:02 +0530

    Add files via upload

--- face_recognition.py
+++ face_recognition.py
@@ -3,9 +3,12 @@ from tkinter import ttk
 from PIL import Image,ImageTk
 from tkinter import messagebox
 import mysql.connector
+from time import strftime
+from datetime import datetime
 import cv2
 import os
 import numpy as np
+
 
 
 class Face_Recognition:
@@ -40,16 +43,25 @@ class Face_Recognition:
 
         b1_1=Button(f_lbl,text="FACE RECOGNITION",cursor="hand2",font=("times new roman",18,"bold"),bg="darkgreen",fg="white",command=self.face_recog)
         b1_1.place(x=365,y=620,width=250,height=40)
-    
 
 
+    def mark_attendance(self,i,r,n,d):
+        with open("shraddha.csv","r+",newline="\n") as f:
+            myDataList=f.readline()
+            name_list=[]
+            for line in myDataList:
+                entry=line.split((","))
+                name_list.append(entry[0])
+            if((i not in name_list) and (r not in name_list)and (n not in name_list)and (d not in name_list) ):
+                now=datetime.now()
+                d1=now.strftime("%d/%m/%Y")
+                dtString=now.strftime("%H:%M:%S")
+                f.writelines(f"\n{i},{r},{n},{d},{dtString},{d1},Present")
 
-# //---------------FACE RECOGNITION----=-------------
-
-
+# //--------------FACE RECOGNITION----=-------------
     def face_recog(self): 
         def draw_boundary(img,classifier,scaleFactor,minNeighbors,color,text,clf):
-            gray_image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+            gray_image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY,320,240)
             features=classifier.detectMultiScale(gray_image,scaleFactor,minNeighbors)
 
             coord=[]
@@ -75,12 +87,19 @@ class Face_Recognition:
                 d=my_cursor.fetchone()
                 d="+".join(d)
 
+                my_cursor.execute("Select Student_id from student where Student_id="+str(id))
+                i=my_cursor.fetchone()
+                i="+".join(i)
 
 
-                if confidence>77:
+
+
+                if confidence>60:
+                    cv2.putText(img,f"ID:{i}",(x,y-75),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
                     cv2.putText(img,f"Roll:{r}",(x,y-55),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
                     cv2.putText(img,f"Name:{n}",(x,y-30),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
                     cv2.putText(img,f"Department:{d}",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
+                    self.mark_attendance(i,r,n,d)
                 else: 
                     cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),3)
                     cv2.putText(img,"Unknown Face",(x,y-5),cv2.FONT_HERSHEY_COMPLEX,0.8,(255,255,255),3)
@@ -96,28 +115,25 @@ class Face_Recognition:
         faceCascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
         clf=cv2.face.LBPHFaceRecognizer_create()
         clf.read("classifier.xml")
+        
 
         video_cap=cv2.VideoCapture(0)
-        if not video_cap.isOpened():
-           print("Error: Could not open webcam.")
-         
-           return
-        print("Webcam opened successfully.")  
 
+        frame_count = 0
         while True:
             ret,img=video_cap.read()
-            if not ret:
-               print("Failed to capture image")
-               break
-            img=recognize(img,clf,faceCascade)
-            cv2.imshow("Welcome to face recognition",img)
+            frame_count += 1
+            if frame_count % 2 == 0:
+               img=cv2.resize(img,(640,480))
+               img=recognize(img,clf,faceCascade)
+               cv2.imshow("Welcome to face recognition",img)
 
             if cv2.waitKey(1)==13:
                 break
         
         video_cap.release()
         cv2.destroyAllWindows()
-
+    
 
 
 if __name__ == "__main__":
